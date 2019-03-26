2cad97ab5d59a637cec7cf67687f6d30b907c21f
Muaykhao86 <waveflow18@gmail.com>
2019-03-26 22:04:15 +0000

    Updates

---
+++ precache-manifest.9dc041c53b85ae442d2ae65a9c0ecb23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3631c4caafcc5106e2b56339f792fdfd",
+    "url": "/RickandMorty/static/media/backgroundBox.3631c4ca.jpg"
+  },
+  {
+    "revision": "df87091ab997e380e2062645e139348b",
+    "url": "/RickandMorty/static/media/Portal.df87091a.gif"
+  },
+  {
+    "revision": "7dcf16cb292fefd591ce",
+    "url": "/RickandMorty/static/js/runtime~main.296dc086.js"
+  },
+  {
+    "revision": "6d3fb1530b245d9f0afe",
+    "url": "/RickandMorty/static/js/main.32691eda.chunk.js"
+  },
+  {
+    "revision": "eb888c496c4ace274aa7",
+    "url": "/RickandMorty/static/js/2.c37d9d9a.chunk.js"
+  },
+  {
+    "revision": "6d3fb1530b245d9f0afe",
+    "url": "/RickandMorty/static/css/main.9b2620ff.chunk.css"
+  },
+  {
+    "revision": "9ddef93a1ebc825ae5bc934fb80064fe",
+    "url": "/RickandMorty/index.html"
+  }
+];
